179e311b292136b5b8357e57a1ddb24c6336e266
Tim@Davis-Dorm-WSL2 <timhwang@ucdavis.edu>
2024-01-24 10:56:28 -0800

    2nd stage

--- app/main.c
+++ app/main.c
@@ -2,14 +2,17 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-int main(int argc, char *argv[]) {
-	// Disable output buffering
-	setbuf(stdout, NULL);
+#define BUFFER_SIZE 4096;
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+int main(int argc, char *argv[]) {
+	setbuf(stdout, NULL);
 	
+	// Set the output and error pipes
+	int out_pipe[2];
+	int err_pipe[2];
+	pipe(out_pipe);
+	pipe(err_pipe);
+
 	char *command = argv[3];
 	int child_pid = fork();
 	if (child_pid == -1) {
@@ -17,13 +20,30 @@ int main(int argc, char *argv[]) {
 	    return 1;
 	}
 	
-	if (child_pid == 0) {
-		// Replace current program with calling program
+	if (child_pid == 0) { // Child process
+		// Redirect the stdout and stderr
+		dup2(out_pipe[1], STDOUT_FILENO);
+		dup2(err_pipe[1], STDERR_FILENO);
+
+		// Close the read end of the pipes
+		close(out_pipe[0]);
+		close(err_pipe[0]);
+
+		// Execute the command
 	    execv(command, &argv[3]);
-	} else {
-		// We're in parent
-		wait(NULL);
-		printf("Child terminated");
+	} 
+	else { // Parent process
+		// Close the write end of the pipes
+		close(out_pipe[1]);
+		close(err_pipe[1]);
+
+		// Read the output and error
+		char out[BUFFER_SIZE];
+		char err[BUFFER_SIZE];
+		read(out_pipe[0], out, sizeof(out));
+		read(err_pipe[0], err, sizeof(err));
+
+		waitpid(child_pid, NULL, 0);
 	}
 
 	return 0;
